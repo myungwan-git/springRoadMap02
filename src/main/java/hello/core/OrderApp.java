64ecab1667b110cdf2ea6755c233e95dbd181031
myungwan-git <nsubway1070635@gmail.com>
2021-10-18 18:04:13 +0900

    20211018 spring roadmap2 POJO - > spring

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
   public static void main(String[] args) {
+    /*
     //MemberService memberService = new MemberServiceImpl();
     //OrderService orderService = new OrderServiceImpl();
     AppConfig appConfig = new AppConfig();
     MemberService memberService = appConfig.memberService();
     OrderService orderService = appConfig.orderService();
+    //위의 코드를 스프링을 사용해 변경해보자. ( AppConfig에서 @Configuration , @Bean 이 있기 전의 코드 )
+    */
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+    OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
     Long memberId = 1L;
     Member member = new Member(memberId, "memberA", Grade.VIP);
